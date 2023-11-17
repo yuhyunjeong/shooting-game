e668f704e7d91fbaf5958fcc3bbefd9c4c9d52df
jade <jadejung95@gmail.com>
2023-11-18 06:33:08 +0900

    make the ship move

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Setting a canvas
+//Set a canvas
 
 let canvas;
 let ctx;
@@ -12,7 +12,7 @@ document.body.appendChild(canvas); //representing canvas in <body>
 let spaceshipX = canvas.width / 2 - 48; // 200 - 48
 let spaceshipY = canvas.height - 96; // 700 - 96
 
-//loading images
+//load images
 let backgroundImage, spaceshipImage, bulletImage, enemyImage, gameOverImage;
 function loadImage() {
   backgroundImage = new Image();
@@ -31,6 +31,41 @@ function loadImage() {
   gameOverImage.src = "img/gameover.png";
 }
 
+// click keyboard
+let keysDown = {}; //Object type
+function setupKeyboardListener() {
+  document.addEventListener("keydown", function (event) {
+    //console.log("what key was pressed?: ", event.keyCode);
+    console.log("what key was pressed?: ", event.key);
+
+    keysDown[event.key] = true; // key-value
+    //console.log("키다운객체에 들어간 값은?:", keysDown)
+  });
+
+  document.addEventListener("keyup", function (event) {
+    delete keysDown[event.key];
+    console.log("버튼 클릭 후", keysDown);
+  });
+}
+
+function update() {
+  // right
+
+  if ("ArrowRight" in keysDown) {
+    // prevent the ship leaving the canvas
+    if (spaceshipX < canvas.width - 96) {
+      spaceshipX += 1; //adjust speed
+    }
+  }
+  // left
+  if ("ArrowLeft" in keysDown) {
+    // prevent the ship leaving the canvas
+    if (spaceshipX > 0) {
+      spaceshipX -= 1;
+    }
+  }
+}
+
 //render : draw UI
 function render() {
   ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height); //image, dx, dy, dWidth, dHeight
@@ -38,10 +73,12 @@ function render() {
 }
 
 function main() {
-  render();
+  update(); // update coordinating values
+  render(); // draw
   console.log("animation calls main function");
   requestAnimationFrame(main); // infinite loop
 }
 
 loadImage();
+setupKeyboardListener();
 main();
