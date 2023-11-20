ec70928589d483b069de6a4fc5c1da9ad9772771
jade <jadejung95@gmail.com>
2023-11-20 18:01:17 +0900

    adjust speed

--- main.js
+++ main.js
@@ -70,7 +70,7 @@ function Enemy() {
     enemyList.push(this);
   };
   this.update = function () {
-    this.y += 2;
+    this.y += 3;
 
     if (this.y >= canvas.height - 64) {
       gameOver = true;
@@ -134,7 +134,7 @@ function createEnemy() {
   const interval = setInterval(function () {
     let e = new Enemy();
     e.init();
-  }, 500); // The setInterval() method calls a function at specified intervals (in milliseconds). function, times(1000ms = 1 second)
+  }, 700); // The setInterval() method calls a function at specified intervals (in milliseconds). function, times(1000ms = 1 second)
 }
 
 function update() {
